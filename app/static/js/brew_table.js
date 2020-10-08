df086272d4085584e243c516f18e7038a418e350
Andrew Berryman <andrew.berryman@gmail.com>
2020-10-08 12:41:57 +0100

    Revert attempt at js

--- app/static/js/brew_table.js
+++ app/static/js/brew_table.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-        var data = JSON.parse(graph_data);
-        $('#table_head:last-child').after('<tr class="' + data.time + '"></tr>');
-        $('.' + data.time).append('<td>' + data.time + '</td>');
-        for (var i = 0; i < data.data.length; i++){
-            self.series[i].addPoint([data.time, data.data[i]]);
-            $('.' + data.time).append('<tr>' + data.data[i] + '</tr>');
-        }
+
     });
